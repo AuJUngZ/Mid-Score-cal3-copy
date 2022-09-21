37ab5947205abefab046a49f669d5e96c2b1e8bc
AuJung <nuttapong6280@gmail.com>
2022-09-22 00:13:54 +0700

    fix aa

--- backendLibs/dbLib.js
+++ backendLibs/dbLib.js
@@ -2,15 +2,16 @@ import fs from "fs";
 import path from "path";
 
 export function readDB() {
-  const str = fs.readFileSync(path.join(process.cwd(), "/db/allscore.json"));
+  const filePath = path.join(process.cwd(), "db");
+  const str = fs.readFileSync(path.join(filePath, "allscore.json"), "utf-8");
   const todolist = JSON.parse(str);
   return todolist;
 }
 
 export function writeDB(score) {
+  const filePath = path.join(process.cwd(), "db", "allscore.json");
   const str = JSON.stringify(score);
-  const file = path.join(process.cwd(), "/db/allscore.json");
-  fs.writeFileSync(file, str, "utf-8");
+  fs.writeFileSync(path.join(filePath, "allscore.json"), str, "utf-8");
 }
 
 export function delDB(id) {}
