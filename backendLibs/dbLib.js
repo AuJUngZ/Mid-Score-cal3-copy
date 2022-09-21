a0c868f313a3b11223bdd96f7488f06c58996ad8
AuJung <nuttapong6280@gmail.com>
2022-09-22 00:00:40 +0700

    fix agin

--- backendLibs/dbLib.js
+++ backendLibs/dbLib.js
@@ -2,14 +2,14 @@ import fs from "fs";
 import path from "path";
 
 export function readDB() {
-  const str = fs.readFileSync(path.join("db", "allscore.json"), "utf-8");
+  const str = fs.readFileSync(path.join(process.cwd(), "/db/allscore.json"));
   const todolist = JSON.parse(str);
   return todolist;
 }
 
 export function writeDB(score) {
   const str = JSON.stringify(score);
-  const file = path.join("db", "allscore");
+  const file = path.join(process.cwd(), "/db/allscore.json");
   fs.writeFileSync(file, str, "utf-8");
 }
 
