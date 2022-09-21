4723becf42a815cc73549b28a90b7e9fdb195018
AuJung <nuttapong6280@gmail.com>
2022-09-21 20:26:11 +0700

    delete

--- pages/api/score/[id].js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { readDB, writeDB } from "../../../backendLibs/dbLib";
-
-export default function Edit(req, res) {
-  if (req.method == "DELETE") {
-    const scores = readDB();
-  }
-}
