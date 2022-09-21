bbd311742373718d9f7330358afbadc36ceef194
AuJung <nuttapong6280@gmail.com>
2022-09-21 21:28:27 +0700

    fix

--- backendLibs/dbLib.js
+++ backendLibs/dbLib.js
@@ -1,12 +1,14 @@
 import fs from "fs";
 
 export function readDB() {
+  const fs = require("fs");
   const srt = fs.readFileSync("./db/allscore.json", "utf-8");
   const todolist = JSON.parse(srt);
   return todolist;
 }
 
 export function writeDB(score) {
+  const fs = require("fs");
   const srt = JSON.stringify(score);
   fs.writeFileSync("./db/allscore.json", srt, "utf-8");
 }
